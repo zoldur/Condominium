79631a53ab65d2eec7ca3cb88e93a31198f351e1
Zoldur <zoldur99@gmail.com>
2018-06-16 03:15:56 -0700

    Get IP

--- condominium_install.sh
+++ condominium_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='Condominium'
 COIN_PORT=33588
 RPC_PORT=33589
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -148,7 +148,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
